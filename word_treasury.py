a2175d1693256467c4d071ce7a71974b023e7e8d
Aryabhatt <bhattsahab314@gmail.com>
2023-08-29 22:43:35 +0530

    Added the word Dictionary and \n in hints_list

--- word_treasury.py
+++ word_treasury.py
@@ -1,19 +1,20 @@
 words_list = ["Meridian", "Grow", "Replant", "Methodize", "Thud", "Lesser", "Spacious", "Cloudburst", "Woken",
               "Suspicion", "Foe", "Recap"]
 
-hints_list = ["Word Category: Noun.\n\n"
-              "It is the great circle passing through the two poles of the celestial sphere and the zenith of a given "
-              "observer.",
-              "Word Category: Verb.\n\nTo increase in size by a natural process.",
-              "Word Category: Verb.\n\nTo plant (something) again.",
-              "Word Category: Verb.\n\nTo make methodical",
-              "Word Category: Noun.\n\nA dull sound, as that of a heavy object striking a solid surface.",
-              "Word Category: Adjective.\n\nSmaller in amount, value, or importance, especially in a comparison "
-              "between two things.",
-              "Word Category: Adjective.\n\nHaving enough or abundant space or room; large in area or extent.",
-              "Word Category: Noun.\n\nA sudden heavy rainstorm; a downpour.",
-              "Word Category: Verb.\n\nTo rouse from sleep.",
-              "Word Category: Noun.\n\nA belief or idea that something may be true",
-              "Word Category: Noun.\n\nAn enemy in war",
-              "Word Category: Noun.\n\nA summary of what has been said"
+hints_list = ["Word Category: Noun.\n"
+              "Definition: It is the great circle passing through the two poles of the celestial sphere and the "
+              "zenith of a given observer.",
+              "Word Category: Verb.\nDefinition: To increase in size by a natural process.",
+              "Word Category: Verb.\nDefinition: To plant (something) again.",
+              "Word Category: Verb.\nDefinition: To make things methodical.",
+              "Word Category: Noun.\nDefinition: A dull sound, as that of a heavy object striking a solid surface.",
+              "Word Category: Adjective.\nDefinition: Smaller in amount, value, or importance, especially in a "
+              "comparison between two things.",
+              "Word Category: Adjective.\nDefinition: Having enough or abundant space or room; large in area or"
+              " extent.",
+              "Word Category: Noun.\nDefinition: A sudden heavy rainstorm; a downpour.",
+              "Word Category: Verb.\nDefinition: To rouse from sleep.",
+              "Word Category: Noun.\nDefinition: A belief or idea that something may be true.",
+              "Word Category: Noun.\nDefinition: An enemy in war.",
+              "Word Category: Noun.\nDefinition:A summary of what has been said."
               ]
