2de4676ccb99e3b5059969171a8723ed0d3cf1f9
Aryabhatt <bhattsahab314@gmail.com>
2023-08-29 21:44:24 +0530

    Added list of words and their respective hints

---
+++ word_treasury.py
@@ -0,0 +1,19 @@
+words_list = ["Meridian", "Grow", "Replant", "Methodize", "Thud", "Lesser", "Spacious", "Cloudburst", "Woken",
+              "Suspicion", "Foe", "Recap"]
+
+hints_list = ["Word Category: Noun.\n\n"
+              "It is the great circle passing through the two poles of the celestial sphere and the zenith of a given "
+              "observer.",
+              "Word Category: Verb.\n\nTo increase in size by a natural process.",
+              "Word Category: Verb.\n\nTo plant (something) again.",
+              "Word Category: Verb.\n\nTo make methodical",
+              "Word Category: Noun.\n\nA dull sound, as that of a heavy object striking a solid surface.",
+              "Word Category: Adjective.\n\nSmaller in amount, value, or importance, especially in a comparison "
+              "between two things.",
+              "Word Category: Adjective.\n\nHaving enough or abundant space or room; large in area or extent.",
+              "Word Category: Noun.\n\nA sudden heavy rainstorm; a downpour.",
+              "Word Category: Verb.\n\nTo rouse from sleep.",
+              "Word Category: Noun.\n\nA belief or idea that something may be true",
+              "Word Category: Noun.\n\nAn enemy in war",
+              "Word Category: Noun.\n\nA summary of what has been said"
+              ]
